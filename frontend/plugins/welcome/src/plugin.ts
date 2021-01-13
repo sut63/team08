a66509e5a3a9b929c9ca52a22b39585b95ad2b41
dhetporn <gopla7789@gmail.com>
2021-01-13 17:01:22 +0700

    แกไข plugin.ts - close #152

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,14 +1,30 @@
 import { createPlugin } from '@backstage/core';
-import WelcomePage from './components/WelcomePage';
-import WatchVideo from './components/WatchVideo'
-import SignIn from './components/SignIn'
+import Regeister from './components/Patient_Register';
+import Rent from './components/Rent_Room';
+//import SignIn from './components/SignIn'
+import Covered from './components/CoveredPerson'
+import homedoctor from './components/homedoctor'
+import homenurse from './components/homenurse'
+import homemedical from './components/homemedical'
+import Prescription from './components/Prescription'
+import Diagnose from './components/Diagnose'
+import Operativerecord from './components/Operativerecord'
 
 
 export const plugin = createPlugin({
   id: 'welcome',
   register({ router }) {
-    router.registerRoute('/', WelcomePage);
-    router.registerRoute('/watch_video', WatchVideo);
-    router.registerRoute('/signin', SignIn);
+    //router.registerRoute('/', SignIn);
+    router.registerRoute('/homedoctor', homedoctor);
+    router.registerRoute('/homenurse', homenurse);
+    router.registerRoute('/homemedical', homemedical);
+    router.registerRoute('/reg', Regeister);
+    router.registerRoute('/rent', Rent);
+    router.registerRoute('/covered', Covered);
+    router.registerRoute('/pre', Prescription);
+    router.registerRoute('/dia', Diagnose);
+    router.registerRoute('/opera', Operativerecord);
+    
+    
   },
 });
