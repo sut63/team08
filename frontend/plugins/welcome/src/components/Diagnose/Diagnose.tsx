130effc1ecb89f7b8342747252750b0277d0029c
OfficialNon <watcharapong3872@gmail.com>
2021-01-21 22:10:56 +0700

    แก้ไขหน้า UI - close #250

--- frontend/plugins/welcome/src/components/Diagnose/Diagnose.tsx
+++ frontend/plugins/welcome/src/components/Diagnose/Diagnose.tsx
@@ -5,7 +5,7 @@ import SaveIcon from '@material-ui/icons/Save'; // icon save
 import { Link as RouterLink } from 'react-router-dom';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Swal from 'sweetalert2'; // alert
-
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import {
   Container,
   Grid,
@@ -13,6 +13,7 @@ import {
   Select,
   InputLabel,
   MenuItem,
+  TextField,
   Button,
   Link,
 } from '@material-ui/core';
@@ -21,6 +22,13 @@ import { EntPatient } from '../../api/models/EntPatient'; // import interface Pa
 import { EntDoctor } from '../../api/models/EntDoctor'; // import interface Doctor
 import { EntDisease} from '../../api/models/EntDisease'; // import interface Disease
 import { EntDepartment } from '../../api/models/EntDepartment'; // import interface Department
+
+// name
+import { Cookies } from 'react-cookie/cjs';//cookie
+import { useCookies } from 'react-cookie/cjs';//cookie
+const cookies = new Cookies();
+const Name = cookies.get('Name');
+
 // header css
 const HeaderCustom = {
   minHeight: '50px',
@@ -60,8 +68,67 @@ const Diagnose: FC<{}> = () => {
   const [doctors, setDoctors] = React.useState<EntDoctor[]>([]);
   const [diseases, setDiseases] = React.useState<EntDisease[]>([]);
   const [departments, setDepartments] = React.useState<EntDepartment[]>([]);
-    
-  // alert setting
+  
+
+  const getPatient = async () => {
+    const res = await http.listPatient({ limit: 2, offset: 0 });
+    setPatients(res);
+  };
+
+  const getDoctor = async () => {
+    const res = await http.listDoctor({ limit: 3, offset: 0 });
+    setDoctors(res);
+  };
+
+  const getDisease = async () => {
+    const res = await http.listDisease({ limit: 8, offset: 0 });
+    setDiseases(res);
+  };
+
+  const getDepartment = async () => {
+    const res = await http.listDepartment({ limit: 6, offset: 0 });
+    setDepartments(res);
+  };
+
+    // Lifecycle Hooks
+    useEffect(() => {
+      getPatient();
+      getDoctor();
+      getDisease();
+      getDepartment();
+    }, []);
+
+  const DiagnoseIDhandleChange = (event: any) => {
+    setDiagnoseID(event.target.value as string);
+  };
+  const DiagnoseSymptomshandleChange = (event: any) => {
+    setDiagnoseSymptoms(event.target.value as string);
+  };
+  const DiagnoseNotehandleChange = (event: any) => {
+    setDiagnoseNote(event.target.value as string);
+  };
+  const PatienthandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setPatientID(event.target.value as number);
+  };
+  const DoctorhandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setDoctorID(event.target.value as number);
+  };
+  const DiseasehandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setDiseaseID(event.target.value as number);
+  };
+  const DepartmenthandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setDepartmentID(event.target.value as number);
+  };
+
+  const [patientID, setPatientID] = React.useState(Number);
+  const [doctorID, setDoctorID] = React.useState(Number);
+  const [diseaseID, setDiseaseID] = React.useState(Number);
+  const [departmentID, setDepartmentID] = React.useState(Number);
+  const [diagnoseid, setDiagnoseID] = React.useState(String);
+  const [diagnosesymptoms, setDiagnoseSymptoms] = React.useState(String);
+  const [diagnosenote, setDiagnoseNote] = React.useState(String);
+
+
   const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
@@ -73,66 +140,54 @@ const Diagnose: FC<{}> = () => {
       toast.addEventListener('mouseleave', Swal.resumeTimer);
     },
   });
-
-  const getPatient = async () => {
-    const res = await http.listPatient({ limit: 8, offset: 0 });
-    setPatients(res);
+  const diagnoses = {
+    note: diagnosenote,
+    disease: diseaseID,
+    doctor: doctorID,
+    patient: patientID,
+    department: departmentID,
+    symptoms: diagnosesymptoms,
+    number: diagnoseid
+  }
+  // alert error
+const aleartMessage = (icon: any, title: any) => {
+  Toast.fire({
+    icon: icon,
+    title: title,
+  });
+}
+// check error 
+const checkCaseSaveError = (field: string) => {
+  switch (field) {
+    case 'Doctor not found':
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณาเลือกชื่อแพทย์");
+      return;
+    case 'Diagnose_ID':
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณากรอก ID การวินิจฉัย ขึ้นต้นด้วย D และตัวเลข 4 ตัว");
+      return;
+    case 'Patient not found':
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณาป้อนชื่อผู้ป่วย");
+      return;
+    case 'Disease not found':
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณาเลือกโรคที่วินิจฉัย");
+      return;
+      case 'Diagnose_Symptoms':
+        aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณากรอกอาการเพิ่มเติม หากไม่มีให้ใช้ -");
+        return;
+    case 'Department not found':
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณาเลือกแผนกที่เข้ารับการรักษา");
+      return;
+    case 'Diagnose_Note':
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ: กรุณากรอกหมายเหตุ หากไม่มีให้ใช้ -");
+      return;
+    default:
+      aleartMessage("error", "บันทึกข้อมูลไม่สำเร็จ");
+      return;
   };
-
-  const getDoctor = async () => {
-    const res = await http.listDoctor({ limit: 8, offset: 0 });
-    setDoctors(res);
-  };
-
-  const getDisease = async () => {
-    const res = await http.listDisease({ limit: 8, offset: 0 });
-    setDiseases(res);
-  };
-
-  const getDepartment = async () => {
-    const res = await http.listDepartment({ limit: 8, offset: 0 });
-    setDepartments(res);
-  };
-
-
-  const PatienthandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setPatientID(event.target.value as number);
-  };
-  const DoctorhandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setDoctorID(event.target.value as number);
-  };
-  const DiseasehandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setDiseaseID(event.target.value as number);
-  };
-
-  const DepartmenthandleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setDepartmentID(event.target.value as number);
-  };
-
-  const [patientID, setPatientID] = React.useState(Number);
-  const [doctorID, setDoctorID] = React.useState(Number);
-  const [diseaseID, setDiseaseID] = React.useState(Number);
-  const [departmentID, setDepartmentID] = React.useState(Number);
-
-
-
-  // Lifecycle Hooks
-  useEffect(() => {
-    getPatient();
-    getDoctor();
-    getDisease();
-    getDepartment();
-  }, []);
-
+}
 
   // function save data
   function save() {
-    const diagnoses = {
-      patient: patientID,
-      doctor: doctorID,
-      disease: diseaseID,
-      department: departmentID,
-    }
     const apiUrl = 'http://localhost:8080/api/v1/diagnoses';
     const requestOptions = {
       method: 'POST',
@@ -152,49 +207,45 @@ const Diagnose: FC<{}> = () => {
             title: 'บันทึกข้อมูลสำเร็จ',
           });
         } else {
-          Toast.fire({
-            icon: 'error',
-            title: 'บันทึกข้อมูลไม่สำเร็จ',
-          });
+          checkCaseSaveError(data.error.Name || data.error)
         }
 
       });
 }
-  
+
+// name
+function a11yProps(index: any) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    'aria-controls': `scrollable-force-tabpanel-${index}`,
+  };
+}
+const [cookies, setCookie, removeCookie] = useCookies(['cookiename']);
+
+  function Logout() {
+    removeCookie('ID', { path: '/' })
+    removeCookie('Name', { path: '/' })
+    removeCookie('Email', { path: '/' })
+    window.location.href = "http://localhost:3000/";
+  }
+
   return (
     <Page theme={pageTheme.service}>
-      <Header style={HeaderCustom} title={`ระบบการวินิจฉัยโรคผู้ป่วยใน`}>
-      <AccountCircleIcon aria-controls="fade-menu" aria-haspopup="true"  fontSize="large" />
+     <Header style={HeaderCustom} title={`ระบบวินิจฉัยโรคผู้ป่วยใน`}>
+     <AccountCircleIcon aria-controls="fade-menu" aria-haspopup="true"  fontSize="large" />
         <div style={{ marginLeft: 10 }}> </div>
+        <div style={{ marginLeft: 1 }}>{Name}</div>
+        <div style={{ marginLeft: 10 }}>
         <Link component={RouterLink} to="/">
              Logout
          </Link>
+         </div>
       </Header>
+
       <Content>
         <Container maxWidth="sm">
           <Grid container spacing={3}>
             <Grid item xs={12}></Grid>
-            <Grid item xs={4}>
-              <div className={classes.paper}>ชื่อผู้ป่วย</div>
-            </Grid>
-            <Grid item xs={8}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel></InputLabel>
-                <Select
-                  name="patient"
-                  value={patientID}
-                  onChange={PatienthandleChange}
-                >
-                  {patients.map(item => {
-                    return (
-                      <MenuItem key={item.id} value={item.id}>
-                        {item.patientName}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
 
             <Grid item xs={4}>
               <div className={classes.paper}>แพทย์ที่รักษา</div>
@@ -211,6 +262,48 @@ const Diagnose: FC<{}> = () => {
                     return (
                       <MenuItem key={item.id} value={item.id}>
                         {item.doctorName}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={4}>
+            <div className={classes.paper}>รหัสการวินิจฉัย</div>
+          </Grid>
+          <Grid item xs={8}>
+          <TextField
+             name="diagnoseID"
+             id="Diagnose_ID"
+             label=""
+             multiline
+             defaultValue="Default Value"
+             variant="outlined"
+             onChange={DiagnoseIDhandleChange}
+             value={diagnoseid} 
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+            />
+          </Grid>
+
+            <Grid item xs={4}>
+              <div className={classes.paper}>ชื่อผู้ป่วย</div>
+            </Grid>
+            <Grid item xs={8}>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel></InputLabel>
+                <Select
+                  name="patient"
+                  value={patientID}
+                  onChange={PatienthandleChange}
+                >
+                  {patients.map(item => {
+                    return (
+                      <MenuItem key={item.id} value={item.id}>
+                        {item.patientName}
                       </MenuItem>
                     );
                   })}
@@ -239,6 +332,27 @@ const Diagnose: FC<{}> = () => {
                 </Select>
               </FormControl>
             </Grid>
+
+            <Grid item xs={4}>
+            <div className={classes.paper}>อาการเพิ่มเติม</div>
+          </Grid>
+          <Grid item xs={8}>
+          <FormControl variant="outlined" className={classes.formControl}>
+          <TextField
+             name="symptoms"
+             id="symptoms"
+             variant="outlined"
+             value={diagnosesymptoms} 
+             onChange={DiagnoseSymptomshandleChange}
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                
+            />
+            </FormControl>
+          </Grid>
+
             <Grid item xs={4}>
               <div className={classes.paper}>แผนกการเข้ารักษา</div>
             </Grid>
@@ -261,7 +375,26 @@ const Diagnose: FC<{}> = () => {
               </FormControl>
             </Grid>
 
-          
+            <Grid item xs={4}>
+            <div className={classes.paper}>หมายเหตุ</div>
+          </Grid>
+          <Grid item xs={8}>
+          <TextField
+             name="note"
+             label=""
+             multiline
+             defaultValue="Default Value"
+             variant="outlined"
+             value={diagnosenote} 
+             onChange={DiagnoseNotehandleChange}
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                
+            />
+          </Grid>
+
             <Grid item xs={4}></Grid>
             <Grid item xs={8}>
             <Button
@@ -273,6 +406,15 @@ const Diagnose: FC<{}> = () => {
                 
               >
                 SAVE
+            </Button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button
+              variant="contained"
+              size="large"
+              startIcon={<NavigateBeforeIcon />}
+              component={RouterLink} to="/homedoctor"
+            >
+              Back
               </Button>
             </Grid>
           </Grid>
@@ -282,4 +424,4 @@ const Diagnose: FC<{}> = () => {
   );
 };
 
-export default Diagnose;
+export default Diagnose; 
